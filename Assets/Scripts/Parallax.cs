fab2fd4ec06063504c9352bb4bbfb2cfe20eef81
Danshicu <Promchuk.danik@gmail.com>
2024-06-17 10:29:09 +0300

    Added player states

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _parallaxStrength;
+    private float _length, _startPos;   
+
+
+    void Start()
+    {
+         _startPos = transform.position.x;
+         _length = GetComponent<SpriteRenderer>().bounds.size.x;    
+    }
+
+    void Update()
+    {
+         float temp = (_camera.transform.position.x * (1-_parallaxStrength));
+         float distance = (_camera.transform.position.x * _parallaxStrength);
+
+         transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
+         if(temp > _startPos + _length) _startPos += _length;
+         else if (temp < _startPos - _length) _startPos -= _length;        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
@@ -12,21 +14,39 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _jumpPower;
     [SerializeField] private Animator _animator;
     [SerializeField] private Rigidbody2D _rigidbody;
-    [SerializeField] private Collider2D _collider2D;
+    [SerializeField] private Collider2D _footCollider;
     private static readonly int IsRunning = Animator.StringToHash("IsRunning");
-    
+    private static readonly int IsFalling = Animator.StringToHash("IsFalling");
+    private static readonly int IsJumping = Animator.StringToHash("IsJumping");
+
     void Update()
     {
         Run();
         FlipSprite();
+        Debug.Log(_rigidbody.velocity.y);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.velocity.y < -4)
+        {
+            _animator.SetBool(IsJumping, false);
+            _animator.SetBool(IsFalling, true);
+        }
+        if(Mathf.Abs(_rigidbody.velocity.y) < 0.5f)
+        {
+            _animator.SetBool(IsFalling, false);
+            _animator.SetBool(IsJumping, false);
+        }
     }
 
     void OnJump(InputValue value)
     {
-        if (!_collider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+        if (!_footCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
         if (value.isPressed)
         {
-            
+            _animator.SetBool(IsJumping, true);
             _rigidbody.velocity += new Vector2(0, _jumpPower);
         }
     }
@@ -49,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
     {
         _rigidbody.velocity = new Vector2(moveInput.x * _runSpeed, _rigidbody.velocity.y);
         
-        bool horizontalMoving = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon;
+        bool horizontalMoving = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon && Mathf.Abs(_rigidbody.velocity.y) > Mathf.Epsilon;
         _animator.SetBool(IsRunning, horizontalMoving);
     }
 }
